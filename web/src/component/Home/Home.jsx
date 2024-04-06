9f9d33b1188c90bf655327853d54a0b59cec704c
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2024-04-06 04:28:25 +0000

    web modif

---
+++ web/src/component/Home/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Home.css";
+import MultitemCarousel from './MultitemCarousel';
+export const Home = () => {
+  
+  return (
+    <div className=''>
+        <section className='banner -z-50 relative flex flex-col justify-center items-center'>
+
+            <div className='w-[50vw] z-10 text-center'>
+                <p className='title text-6xl lg:text-6xl z-10 py-5'>SkyLine</p>
+                <p className='z-10 text-gray-300 text-xl lg:text-3xl'>Where Journeys Begin and Connections Flourish</p>
+
+            </div>
+
+            <div className='cover absolute top-0 left-0 right-0'>
+
+
+            </div>
+
+            <div className='fadout'>
+
+            </div>
+        </section>
+        <section className='p-10 lg:py-10 lg:px-20'>
+          <p className='text-2xl font-semibold text-gray-400 py-3 pb-10'>Top Flight Destinations</p>
+          <MultitemCarousel/>
+        </section>
+        <section className='px-5 lg:px-20'>
+          <h1 className='text-2xl font-semibold text-gray-400 py-3'>Flight Compagnies</h1>
+          <div>
+            
+          </div>
+        </section>
+
+    </div>
+  )
+}
+
--- web/src/component/Navbar/Navbar.jsx
+++ web/src/component/Navbar/Navbar.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
-import logo from '../Navbar/assets/logo.svg';
-
+import logo from '../Navbar/assets/logo.webp';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Badge from '@mui/material/Badge';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AirplaneTicketIcon from '@mui/icons-material/AirplaneTicket';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import "./Navbar.css";
 
 export const Navbar = () => {
   return (
-    <div className='px-5 z-50 py-[.8rem] bg-[#00d1cd] lg:px-20 flex justify-between '>
+    <div className='px-5 z-50 py-[.8rem] bg-[#158a88] lg:px-20 flex justify-between '>
         
         <div className='flex  items-center space-x-4'>
 
             <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
                 <img src={logo} className="App-logo" alt="SkyLine Logo"/>
             </div>
+        </div>
+        <div className='flex items-center space-x-2 lg:space-x-10'>
+
+        <div className=''>
+            <IconButton >
+              <ManageSearchIcon/>
+            </IconButton>
+          </div>
+
+          <div className=''>
+            <IconButton>
+              <Badge color="secondary" badgeContent={3}>
+                <AirplaneTicketIcon/>
+              </Badge>
+            </IconButton>
+          </div>
+          <div className=''>
+            <Avatar sx={{bgcolor:"white",color:"#158a88"}}>
+              <AccountCircleIcon/>
+            </Avatar>
+          </div>
         </div>
     </div>
   )
