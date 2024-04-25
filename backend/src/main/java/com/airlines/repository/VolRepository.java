3dfa5bef3b1a5a959dba33b205475daf78e17935
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2024-04-25 21:56:38 +0100

    volrepository

--- backend/src/main/java/com/airlines/repository/VolRepository.java
+++ backend/src/main/java/com/airlines/repository/VolRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VolRepository extends JpaRepository<Vol, Long> {
-    List<Vol>findbyDestination(Ville nom);
-    List<Vol>findbyPrice(Float prix);
+    List<Vol> findVolByAeroportsVille(Ville ville);
+
+    List<Vol>findVolByPrix(Float prix);
 
 }
