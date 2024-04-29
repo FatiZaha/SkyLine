9dc40c2b3cd853ed11e729ca2306e4b563361c5e
Hajar_Guelbaoui <132174662+Guelbaoui@users.noreply.github.com>
2024-04-29 15:51:38 +0430

    mobile

---
+++ skylineapp/app/src/main/java/com/example/skylineapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.skylineapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
