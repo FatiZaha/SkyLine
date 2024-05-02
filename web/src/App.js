d3a9ed6aa30792dc2e4c591faa3f158e8b86b6f0
dohatir <dohagit0@gmail.com>
2024-05-02 16:58:10 +0100

    Routing

--- web/src/App.js
+++ web/src/App.js
@@ -1,32 +1,44 @@
-
 import './App.css';
-
+import { BrowserRouter, Routes, Route, Redirect, Outlet, useLocation } from 'react-router-dom';
 import { Navbar } from './component/User/Navbar/Navbar';
 import Home from './component/User/Home/Home';
-import { CssBaseline,ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { darkTheme } from './Theme/DarkTheme';
 import FlightsFilter from './component/User/Flights/FlightsFilter';
 import SignIn from './component/SignIn/SignIn';
 import SignUp from './component/SignUp/SignUp';
 import Sidebar from './component/Admin/Dashboard/Sidebar';
- 
+import { useState } from 'react';
+
 function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+function AppRouter() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === '/signin' || location.pathname === '/signup';
 
   return (
-    <ThemeProvider theme={darkTheme}>
-
-    {/* <SignIn/>
-      <SignUp/>
-      <Home/>
-      <CssBaseline/>
-    <Navbar/>
-     
-      <FlightsFilter />
-      */}
-
-       <Sidebar/> 
-
-    </ThemeProvider>
+    <>
+      {!hideNavbar && <Navbar />} {/* Afficher la Navbar sauf dans les pages "SignIn" et "SignUp" */}
+      <Outlet />
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/signin" element={<SignIn />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/filter" element={<FlightsFilter />} />
+        <Route exact path="/sidebar" element={<Sidebar />} />
+      </Routes>
+    </>
   );
 }
 
