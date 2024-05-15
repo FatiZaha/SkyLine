02b6c17061b9596837238bf0844ebc9206a85480
Hajar_Guelbaoui <132174662+Guelbaoui@users.noreply.github.com>
2024-05-15 20:35:20 +0430

    update

--- skylineapp/app/src/main/java/com/example/skylineapp/MainActivity.java
+++ skylineapp/app/src/main/java/com/example/skylineapp/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.skylineapp;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private Button viewFlightsButton;
-
+    private ImageView homeButton;
+    private ImageView searchButton;
+    private ImageView cartButton;
+    private ImageView profileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Action à effectuer lors du clic sur le bouton "View Available Flights"
                 Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Créer un Intent pour démarrer MainActivity
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+         searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FlightsActivity.class);
+                startActivity(intent);
+            }
+        });
+        cartButton = findViewById(R.id.cart_button);
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Créer un Intent pour démarrer la nouvelle activité représentant la page de réservation
+                Intent intent = new Intent(MainActivity.this, ReservationActivity.class);
+                startActivity(intent);
+            }
+        });
+        profileButton = findViewById(R.id.profile_button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Créer un Intent pour démarrer la nouvelle activité représentant la page de profil
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
             }
         });
     }
--- skylineapp/app/src/main/java/com/example/skylineapp/ProfileActivity.java
+++ skylineapp/app/src/main/java/com/example/skylineapp/ProfileActivity.java
@@ -1,11 +1,13 @@
 package com.example.skylineapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.content.Intent;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -15,6 +17,10 @@ public class ProfileActivity extends AppCompatActivity {
     private EditText emailEditText;
     private Button saveButton;
 
+    private ImageView homeButton;
+    private ImageView searchButton;
+    private ImageView cartButton;
+    private ImageView profileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +51,41 @@ public class ProfileActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
 
                 // Perform necessary actions with the edited profile data
+            }
+        });
+        homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, FlightsActivity.class);
+                startActivity(intent);
+            }
+        });
+        cartButton = findViewById(R.id.cart_button);
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProfileActivity.this, ReservationActivity.class);
+                startActivity(intent);
+            }
+        });
+        profileButton = findViewById(R.id.profile_button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProfileActivity.this, ProfileActivity.class);
+                startActivity(intent);
             }
         });
     }
